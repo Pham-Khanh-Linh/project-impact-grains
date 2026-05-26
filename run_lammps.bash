5363c680c75a0a83a9b65a47c38dfb27f4d87f00
Pham-Khanh-Linh <phamkhanhlinh.fr@gmail.com>
2026-05-26 09:51:22 +0100

    first commit for LAMMPS

---
+++ run_lammps.bash
@@ -0,0 +1,15 @@
+DATA_DIR="data"
+
+if [ ! -d "$DATA_DIR" ]; then
+    mkdir -p "$DATA_DIR"
+    echo "data directory created."
+else
+    echo "data directory already exists."
+fi
+
+# #### RUN with OPENMP
+export OMP_NUM_THREADS=4
+../../lammps_builds/lmp_stable_22052025_update4 -in in.condition.initiale
+# #### RUN with MPI
+# mpiexec -n 2 ../../lammps_builds/lmp_stable_22052025_update4 -in in.pour.grains
+
