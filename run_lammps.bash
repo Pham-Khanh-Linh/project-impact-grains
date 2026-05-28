8342a8553e22b5260b78a0517d6788e9efcb8c9e
Pham-Khanh-Linh <phamkhanhlinh.fr@gmail.com>
2026-05-28 12:50:41 +0100

    1er simu est plus stable, MAJ avec succes 2eme simu

--- run_lammps.bash
+++ run_lammps.bash
@@ -12,4 +12,3 @@ export OMP_NUM_THREADS=4
 ../../lammps_builds/lmp_stable_22052025_update4 -in in.impact
 # #### RUN with MPI
 # mpiexec -n 2 ../../lammps_builds/lmp_stable_22052025_update4 -in in.pour.grains
-
